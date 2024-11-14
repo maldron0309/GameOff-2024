1a5f342c97509b1e184a89ab8b294dee6786f6c8
bryengx <janisgx@gmail.com>
2024-11-14 16:01:59 +0200

    added character animation

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -6,9 +6,10 @@ using UnityEngine.InputSystem;
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
-
+    public Animator anim;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private bool faceRight = true;
 
     private void Start()
     {
@@ -18,6 +19,23 @@ public class PlayerMove : MonoBehaviour
     private void Update()
     {
         rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+        if(Mathf.Abs(rb.velocity.x) > 0.01f)
+        {
+            anim.Play("Move");
+            if (rb.velocity.x > 0 && !faceRight)
+                Flip();
+            if (rb.velocity.x < 0 && faceRight)
+                Flip();
+        }
+        else
+        {
+            anim.Play("Idle");
+        }
+    }
+    public void Flip()
+    {
+        anim.gameObject.transform.localScale = new Vector3(-anim.gameObject.transform.localScale.x, anim.gameObject.transform.localScale.y, anim.gameObject.transform.localScale.z);
+        faceRight = !faceRight;
     }
 
     public void OnMove(InputAction.CallbackContext context)
